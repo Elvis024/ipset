734bb52d22d95e2c69cb8ff1842bcc7f54a278fc
elvis <asongafac.elvis@yahoo.com>
2021-04-22 00:10:09 -0400

    made pu

--- ecr_credentials.sh
+++ ecr_credentials.sh
@@ -2,11 +2,7 @@
 
 mkdir -p ~/.aws
 
-cat > ~/.aws/credentials << EOL
-[default]
-aws_access_key_id= $AWS_ACCESS_KEY_ID
-aws_secret_access_key= $AWS_SECRETE_ACCESS_KEY
-EOL
+
 
 cat > ~/.aws/config << EOL
 [default]
