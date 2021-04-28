1f6c0ab95678368ac7d047fb43b862edecd3b46a
elvis <asongafac.elvis@yahoo.com>
2021-04-28 15:18:40 -0400

    modified iplistsssse

--- aws_ipset.py
+++ aws_ipset.py
@@ -7,7 +7,7 @@ x=[]
 print(x)
 
 IP_LIST = [
-    {'ip_set_name':'DEFAULT-AdminWhitelistSetIPV4','ip_set_id':'6ff6f231-8eca-48ea-803b-12152aaa33c7','text_file':'Admin-Team-IPs.json'}
+    {'ip_set_name':'Test','ip_set_id':'e35fbeff-b614-47ed-b5e4-9b2c1bc7ee4d','text_file':'Admin-Team-IPs.json'}
     # {'ip_set_name':'Bastion-host','ip_set_id':'f94afb0e-d2f6-4657-a6f4-a03e31859b33','text_file':'Bastion-host.txt'},
     # {'ip_set_name':'DevTeam-IPs','ip_set_id':'72219b31-418f-4fde-baf5-c4c159b15f7b','text_file':'DevTeam-IPs.txt'},
     # {'ip_set_name':'IP-Blacklist','ip_set_id':'e7ebe92e-d0dc-40f1-a981-b12b629fe761','text_file':'IP-Blacklist.txt'},
